2958f677706cdcc5c36fa3f8612b54e41e3b6972
Kinser <chymzyl2751@gmail.com>
2026-04-11 20:42:59 +0800

    refactor(receive): collapse LayoutInner/LayoutContent into single observer component

--- client/src/pages/wms/station/instances/receive/layout.tsx
+++ client/src/pages/wms/station/instances/receive/layout.tsx
@@ -4,95 +4,42 @@ import { useTranslation } from "react-i18next"
 
 import type { OperationProps } from "@/pages/wms/station/instances/types"
 import { WorkStationView } from "@/pages/wms/station/event-loop/types"
-import { observer, useWorkStation } from "@/pages/wms/station/state"
-import { MessageType } from "@/pages/wms/station/widgets/message"
+import { observer } from "@/pages/wms/station/state"
 
 import ComponentWrapper from "../../component-wrapper"
 import { OPERATION_MAP } from "./config"
 import { StationOperationType } from "./types"
-import { WAREHOUSE_CODE } from "./constants"
 import { valueFilter as scanInfoFilter } from "./operations/tips"
 import ContainerHandler from "./operations/containerHandler"
 import SkuHandler from "./operations/skuHandler"
 import OrderHandler from "./operations/orderHandler"
-import { useReceiveState } from "./hooks"
-import { receiveApiService, createApiHandler } from "./services/api"
+import { useReceiveWorkflow } from "./hooks"
 
 interface ReplenishLayoutProps extends OperationProps<any, any> {
     workStationEvent: WorkStationView<any>
 }
 
-const LayoutInner = (props: ReplenishLayoutProps) => {
-    const state = useReceiveState()
-    return <LayoutContent {...props} {...state} />
-}
+const ReceiveLayout = observer((props: ReplenishLayoutProps) => {
+    const { t } = useTranslation()
+    const {
+        orderNo, setOrderNo,
+        orderInfo,
+        currentSkuInfo,
+        focusValue,
+        setFocusValue,
+        onScanSubmit,
+        onSkuChange,
+        onConfirm,
+        store,
+        onActionDispatch
+    } = useReceiveWorkflow()
 
-const LayoutContent = observer((props: ReplenishLayoutProps & ReturnType<typeof useReceiveState>) => {
-    const {store, message, onActionDispatch} = useWorkStation()
-    const {t} = useTranslation()
-
-    // 早期返回处理
     if (!store?.workStationEvent) {
         return <div>{t("common.loading")}</div>
     }
 
-    const workStationEvent = store.workStationEvent
-    const {
-        orderNo, setOrderNo,
-        orderInfo, setOrderInfo,
-        currentSkuInfo, setCurrentSkuInfo,
-        focusValue, setFocusValue
-    } = props
-    const hasOrder = workStationEvent?.hasOrder ?? ""
+    const hasOrder = store.workStationEvent?.hasOrder ?? ""
 
-    // API错误处理
-    const handleApiError = (error: any) => {
-        message?.({
-            type: MessageType.ERROR,
-            content: error.message
-        })
-    }
-
-    const apiHandler = createApiHandler(handleApiError)
-
-    const onScanSubmit = async () => {
-        await apiHandler(async () => {
-            const orderData = await receiveApiService.queryPlan(orderNo, WAREHOUSE_CODE!)
-            setOrderInfo(orderData)
-            setFocusValue("sku")
-        })
-    }
-
-    const onSkuChange = (detail: any) => {
-        setCurrentSkuInfo(detail)
-        setFocusValue("container")
-    }
-
-    const onConfirm = async ({ containerCode, containerSpecCode, containerId, activeSlot, inputValue }: any) => {
-        await apiHandler(async () => {
-            const res = await receiveApiService.acceptPlan({
-                inboundPlanOrderId: orderInfo.id,
-                inboundPlanOrderDetailId: currentSkuInfo.id,
-                warehouseCode: WAREHOUSE_CODE!,
-                qtyAccepted: inputValue,
-                skuId: currentSkuInfo.skuId,
-                targetContainerCode: containerCode,
-                targetContainerSpecCode: containerSpecCode,
-                targetContainerSlotCode: activeSlot[0],
-                batchAttributes: {},
-                targetContainerId: containerId,
-                workStationId: workStationEvent.workStationId
-            })
-            
-            if (res.status === 200) {
-                if (hasOrder) onScanSubmit()
-                setCurrentSkuInfo({})
-                setFocusValue("sku")
-            }
-        })
-    }
-
-    // 渲染扫描订单界面
     const renderScanOrderView = () => (
         <div className="w-full h-full d-flex flex-col justify-center items-center">
             <div className="w-1/3">
@@ -110,7 +57,6 @@ const LayoutContent = observer((props: ReplenishLayoutProps & ReturnType<typeof 
         </div>
     )
 
-    // 渲染主工作界面
     const renderWorkView = () => (
         <Row className="h-full" justify="space-between" gutter={16}>
             {hasOrder && orderInfo && (
@@ -153,4 +99,4 @@ const LayoutContent = observer((props: ReplenishLayoutProps & ReturnType<typeof 
     )
 })
 
-export default LayoutInner
+export default ReceiveLayout
